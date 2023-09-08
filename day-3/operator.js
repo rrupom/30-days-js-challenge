32e8cabf737ed140b0765fb0e88bccbc1c727863
rrupom <rakib.t.rupom@gmail.com>
2023-09-08 18:09:17 +0600

    day 3 practice completed

---
+++ day-3/operator.js
@@ -0,0 +1,7 @@
+console.log(NaN == NaN);
+console.log(NaN === NaN);
+console.log(null == null);
+console.log(null === null);
+console.log(typeof null);
+console.log(typeof NaN);
+console.log(null == "");
